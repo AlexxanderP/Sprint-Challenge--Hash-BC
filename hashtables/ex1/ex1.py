c33176e9074f8309ec7ee6425e72343d5684fd5d
AlexxanderP <AlexxanderP@users.noreply.github.com>
2019-12-06 10:50:34 -0800

    cleaned up ex1 file, and completed ex2, passing all tests, mvp complete, letting miner continue to run through lunch to get the 100 coin stretch

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -11,33 +11,23 @@ def get_indices_of_item_weights(weights, length, limit):
 
 
     for i in range(len(weights)):
-        # print(weights[i])
-        # print(limit)
         hash_table_insert(ht, weights[i], i)
 
     for i in range(len(weights)):
         new_value = limit - weights[i]
         checker = None
-        # print(new_value)
-        # print(f'LINKED PAIR {linked_pair}')
+
         if hash_table_retrieve(ht, new_value):
             checker = i
             new_value = hash_table_retrieve(ht, new_value)
-            # print(checker)
-            # print(hash_table_retrieve(ht, new_value))
+
             if checker > new_value:
                 return (checker, new_value)
             else:
                 return (new_value, checker)
-        # print(checker)
-        # print(new_value)
+
 
 
     return None
 
 
-def print_answer(answer):
-    if answer is not None:
-        print(str(answer[0] + " " + answer[1]))
-    else:
-        print("None")
